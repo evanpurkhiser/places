6e37d05ec57cc6e99e846332ff91ff49ddd07be1
Evan Purkhiser <git@evanpurkhiser.com>
2026-09-18 15:53:25 -0400

    Include sector in query documentation expectations

--- packages/server/src/rpc/query.test.ts
+++ packages/server/src/rpc/query.test.ts
@@ -30,7 +30,12 @@ it('serves serializable documentation for the registered query capabilities', as
     'has',
     'location',
   ]);
-  expect(documentation.functions.map(fn => fn.name)).toEqual(['point', 'radius', 'rect']);
+  expect(documentation.functions.map(fn => fn.name)).toEqual([
+    'point',
+    'radius',
+    'rect',
+    'sector',
+  ]);
 
   for (const filter of documentation.filters) {
     expect(filter.description).not.toBe('');
